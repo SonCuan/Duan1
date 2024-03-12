2e3199987ee2d4f441940b96d618f62d67f76211
SonCuan <sonquan2611@gmail.com>
2024-03-12 14:15:26 +0700

    add

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
-<?php 
+<?php
 include_once "view/header.php";
-if(isset($_GET['act'])&& $_GET['act'] !=""){
+if (isset($_GET['act']) && $_GET['act'] != "") {
     switch ($_GET['act']) {
         case 'home':
             include_once "view/home.php";
@@ -17,12 +17,18 @@ if(isset($_GET['act'])&& $_GET['act'] !=""){
         case "blog":
             include_once "view/blog.php";
             break;
+        case "login":
+            include_once "view/login.php";
+            break;
+        case "gioithieu":
+            include_once "view/gioithieu.php";
+            break;
         default:
-           ///
+            ///
             echo "<h1>Fooder not file 404</h1>";
             break;
     }
-}else{
+} else {
     include_once "view/home.php";
 }
 include_once "view/footer.php";
--- view/header.php
+++ view/header.php
@@ -204,28 +204,10 @@
                                     class="icon-magnifier"></i><span>Search</span></a>
                             <form action="#" method="get">
                                 <div class="header-search-wrapper">
-                          body          <input type="search" class="form-control" name="q" id="q"
+                                    <input type="search" class="form-control" name="q" id="q"
                                         placeholder="I'm searching for..." required>
                                     <div class="select-custom">
-                                        <select id="cat" name="cat">
-                                            <option value="">All Categories</option>
-                                            <option value="4">Fashion</option>
-                                            <option value="12">- Women</option>
-                                            <option value="13">- Men</option>
-                                            <option value="66">- Jewellery</option>
-                                            <option value="67">- Kids Fashion</option>
-                                            <option value="5">Electronics</option>
-                                            <option value="21">- Smart TVs</option>
-                                            <option value="22">- Cameras</option>
-                                            <option value="63">- Games</option>
-                                            <option value="7">Home &amp; Garden</option>
-                                            <option value="11">Motors</option>
-                                            <option value="31">- Cars and Trucks</option>
-                                            <option value="32">- Motorcycles &amp; Powersports</option>
-                                            <option value="33">- Parts &amp; Accessories</option>
-                                            <option value="34">- Boats</option>
-                                            <option value="57">- Auto Tools &amp; Supplies</option>
-                                        </select>
+                             
                                     </div><!-- End .select-custom -->
                                     <button class="btn icon-magnifier p-0" title="search" type="submit"></button>
                                 </div><!-- End .header-search-wrapper -->
@@ -252,17 +234,16 @@
                                     <a href="index.php">Trang chủ</a>
                                 </li>
                                 <li>
-                                    <a href="?act=shop">Thể loại</a>
+                                    <a href="?act=shop">Sản phẩm</a>
                                     <div class="megamenu megamenu-fixed-width megamenu-3cols">
-                                        <div class="row">
-    
+                                        <div class="row">   
                                         </div>
                                     </div><!-- End .megamenu -->
 
                                     
                                 </li>
                                 <li class="d-none d-xl-block">
-                                    <a href="#">Giới thiệu</a>
+                                    <a href="?act=gioithieu">Giới thiệu</a>
                                   
                                 </li>
                                 
