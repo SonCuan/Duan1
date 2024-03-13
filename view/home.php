741071b462b18a71809d88d89ebf64c99312c353
SonCuan <sonquan2611@gmail.com>
2024-03-13 13:49:21 +0700

    update home

--- view/home.php
+++ view/home.php
@@ -47,115 +47,7 @@
                     <p class="section-description text-center mb-3 appear-animate" data-animation-name="fadeInUpShorter"
                         data-animation-delay="400">Tận hưởng chất lượng vượt trội trong tất cả các sản phẩm của chúng tôi
                     </p>
-
-                    <div class="row mb-2">
-                        <div class="col-md-8 col-lg-6">
-                            <div class="banner banner1 mb-2 appear-animate" data-animation-name="fadeIn"
-                                data-animation-delay="200" style="background-color: #d8d8d8;">
-                                <figure>
-                                    <img src="assets/images/demoes/demo23/banners/banner-1.jpg" alt="banner" width="580"
-                                        height="374">
-                                </figure>
-
-                                <div class="banner-layer d-flex flex-column align-items-end justify-content-center">
-                                    <div class="text-justify">
-                                        <h3 class="font4 font-weight-bold ls-n-25 text-uppercase mb-0">Discounts</h3>
-                                        <h4 class="ls-n-25 text-uppercase">Up to 70%</h4>
-                                        <a href="demo23-shop.html" class="btn btn-link"><i>View our Deals</i><i
-                                                class="icon-right-open-big"></i></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4 col-lg-3">
-                            <div class="banner banner2 mb-2 appear-animate" data-animation-name="fadeIn"
-                                data-animation-delay="200" style="background-color: #f0f5f8;">
-                                <figure>
-                                    <img src="assets/images/demoes/demo23/banners/banner-2.jpg" alt="banner" width="280"
-                                        height="374">
-                                </figure>
-
-                                <div class="banner-layer d-flex flex-column align-items-center justify-content-end">
-                                    <h2 class="ls-n-25 text-uppercase">Summer classics</h2>
-                                    <a href="demo23-shop.html" class="btn btn-link"><i>View our Classics</i><i
-                                            class="icon-right-open-big"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-6 col-lg-3">
-                            <div class="banner banner3 mb-2 appear-animate" data-animation-name="fadeIn"
-                                data-animation-delay="200" style="background-color: #ededed;">
-                                <figure>
-                                    <img src="assets/images/demoes/demo23/banners/banner-3.jpg" alt="banner" width="280"
-                                        height="374">
-                                </figure>
-
-                                <div class="banner-layer d-flex flex-column align-items-center justify-content-between">
-                                    <h2 class="font-weight-bold ls-n-25 text-center text-uppercase">Mother &amp;
-                                        Daughter</h2>
-                                    <a href="demo23-shop.html" class="btn btn-link"><i>View our Outfits</i><i
-                                            class="icon-right-open-big"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-6 col-lg-3">
-                            <div class="banner banner4 mb-2 appear-animate" data-animation-name="fadeIn"
-                                data-animation-delay="200" style="background-color: #f5f6f8;">
-                                <figure>
-                                    <img src="assets/images/demoes/demo23/banners/banner-4.jpg" alt="banner" width="560"
-                                        height="374">
-                                </figure>
-
-                                <div class="banner-layer d-flex flex-column align-items-center">
-                                    <h2 class="ls-n-25 text-uppercase">Babies Outfits</h2>
-                                    <a href="demo23-shop.html" class="btn btn-link"><i>View our Outfits</i><i
-                                            class="icon-right-open-big"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4 col-lg-3">
-                            <div class="banner banner5 mb-2 appear-animate" data-animation-name="fadeIn"
-                                data-animation-delay="200" style="background-color: #f5ecef;">
-                                <figure>
-                                    <img src="assets/images/demoes/demo23/banners/banner-5.jpg" alt="banner" width="280"
-                                        height="374">
-                                </figure>
-
-                                <div class="banner-layer d-flex flex-column align-items-center justify-content-between">
-                                    <h2 class="ls-n-25 text-uppercase">Babies dresses</h2>
-                                    <a href="demo23-shop.html" class="btn btn-link"><i>View our Dresses</i><i
-                                            class="icon-right-open-big"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-8 col-lg-6">
-                            <div class="banner banner6 mb-2 appear-animate" data-animation-name="fadeIn"
-                                data-animation-delay="200" style="background-color: #e2e2e2;">
-                                <figure>
-                                    <img src="assets/images/demoes/demo23/banners/banner-6.jpg" alt="banner" width="280"
-                                        height="374">
-                                </figure>
-
-                                <div class="banner-layer d-flex flex-column align-items-center justify-content-between">
-                                    <h2 class="ls-n-25 text-uppercase">Shoes for Babies</h2>
-                                    <a href="demo23-shop.html" class="btn btn-link"><i>View our Deals</i><i
-                                            class="icon-right-open-big"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                        <!-- Phần thêm vào chữ  -->
-            <h2 class="section-title text-center text-uppercase appear-animate"
-                        data-animation-name="fadeInUpShorter" data-animation-delay="200">Chanel
-                        </h2>
-                    <p style="font-style:italic;" class="section-description text-center mb-3 appear-animate" data-animation-name="fadeInUpShorter"
-                        data-animation-delay="400">"Hương thơm cao cấp, sang trọng, huyền bí"
-                    </p>
-
-
-                   <!--End phần thêm chữ vào index  -->
-
+                            <!-- Phần img  -->
                     <div class="row appear-animate" data-animation-name="fadeInUpShorter" data-animation-delay="400">
                         <div class="col-6 col-md-4 col-xl-3">
                             <div class="product-default inner-quickview inner-icon">
@@ -200,6 +92,151 @@
                                 </div><!-- End .product-details -->
                             </div>
                         </div>
+                        <div class="col-6 col-md-4 col-xl-3">
+                            <div class="product-default inner-quickview inner-icon">
+                                <figure>
+                                    <a href="demo23-product.html">
+                                        <img src="assets/images/demoes/demo23/products/product-2.jpg" width="217"
+                                            height="217" alt="product">
+                                    </a>
+                                    <div class="label-group">
+                                        <div class="product-label label-hot">HOT</div>
+                                    </div>
+                                    <div class="btn-icon-group">
+                                        <a href="demo23-product.html" class="btn-icon btn-add-cart"><i
+                                                class="fa fa-arrow-right"></i></a>
+                                    </div>
+                                    <a href="ajax/product-quick-view.html" class="btn-quickview"
+                                        title="Quick View">Quick
+                                        View</a>
+                                </figure>
+                                <div class="product-details">
+                                    <div class="category-wrap">
+                                        <div class="category-list">
+                                            <a href="demo23-shop.html" class="product-category">category</a>
+                                        </div>
+                                        <a href="wishlist.html" title="Add to Wishlist" class="btn-icon-wish"><i
+                                                class="icon-heart"></i></a>
+                                    </div>
+                                    <h3 class="product-title">
+                                        <a href="demo23-product.html">Porto Extended Camera</a>
+                                    </h3>
+                                    <div class="ratings-container">
+                                        <div class="product-ratings">
+                                            <span class="ratings" style="width:0%"></span><!-- End .ratings -->
+                                            <span class="tooltiptext tooltip-top"></span>
+                                        </div><!-- End .product-ratings -->
+                                    </div><!-- End .product-container -->
+                                    <div class="price-box">
+                                        <span class="product-price">$599.00</span>
+                                    </div><!-- End .price-box -->
+                                </div><!-- End .product-details -->
+                            </div>
+                        </div>
+                        <div class="col-6 col-md-4 col-xl-3">
+                            <div class="product-default inner-quickview inner-icon">
+                                <figure>
+                                    <a href="demo23-product.html">
+                                        <img src="assets/images/demoes/demo23/products/product-3.jpg" width="217"
+                                            height="217" alt="product">
+                                        <img src="assets/images/demoes/demo23/products/product-1.jpg" width="217"
+                                            height="217" alt="product">
+                                    </a>
+                                    <div class="label-group">
+                                        <div class="product-label label-hot">HOT</div>
+                                    </div>
+                                    <div class="btn-icon-group">
+                                        <a href="demo23-product.html" class="btn-icon btn-add-cart"><i
+                                                class="fa fa-arrow-right"></i></a>
+                                    </div>
+                                    <a href="ajax/product-quick-view.html" class="btn-quickview"
+                                        title="Quick View">Quick
+                                        View</a>
+                                </figure>
+                                <div class="product-details">
+                                    <div class="category-wrap">
+                                        <div class="category-list">
+                                            <a href="demo23-shop.html" class="product-category">category</a>
+                                        </div>
+                                        <a href="wishlist.html" title="Add to Wishlist" class="btn-icon-wish"><i
+                                                class="icon-heart"></i></a>
+                                    </div>
+                                    <h3 class="product-title">
+                                        <a href="demo23-product.html">Blue Child Overshoes</a>
+                                    </h3>
+                                    <div class="ratings-container">
+                                        <div class="product-ratings">
+                                            <span class="ratings" style="width:80%"></span><!-- End .ratings -->
+                                            <span class="tooltiptext tooltip-top"></span>
+                                        </div><!-- End .product-ratings -->
+                                    </div><!-- End .product-container -->
+                                    <div class="price-box">
+                                        <span class="product-price">$39.00</span>
+                                    </div><!-- End .price-box -->
+                                </div><!-- End .product-details -->
+                            </div>
+                        </div>
+                        <div class="col-6 col-md-4 col-xl-3">
+                            <div class="product-default inner-quickview inner-icon">
+                                <figure>
+                                    <a href="demo23-product.html">
+                                        <img src="assets/images/demoes/demo23/products/product-4.jpg" width="217"
+                                            height="217" alt="product">
+                                    </a>
+                                    <div class="label-group">
+                                        <div class="product-label label-hot">HOT</div>
+                                        <div class="product-label label-sale">-13%</div>
+                                    </div>
+                                    <div class="btn-icon-group">
+                                        <a href="#" title="Add To Cart"
+                                            class="btn-icon btn-add-cart product-type-simple"><i
+                                                class="icon-shopping-cart"></i></a>
+                                    </div>
+                                    <a href="ajax/product-quick-view.html" class="btn-quickview"
+                                        title="Quick View">Quick
+                                        View</a>
+                                </figure>
+                                <div class="product-details">
+                                    <div class="category-wrap">
+                                        <div class="category-list">
+                                            <a href="demo23-shop.html" class="product-category">category</a>
+                                        </div>
+                                        <a href="wishlist.html" title="Add to Wishlist" class="btn-icon-wish"><i
+                                                class="icon-heart"></i></a>
+                                    </div>
+                                    <h3 class="product-title">
+                                        <a href="demo23-product.html">Baby Summer Underclothes</a>
+                                    </h3>
+                                    <div class="ratings-container">
+                                        <div class="product-ratings">
+                                            <span class="ratings" style="width:0%"></span><!-- End .ratings -->
+                                            <span class="tooltiptext tooltip-top"></span>
+                                        </div><!-- End .product-ratings -->
+                                    </div><!-- End .product-container -->
+                                    <div class="price-box">
+                                        <span class="old-price">$299.00</span>
+                                        <span class="product-price">$259.00</span>
+                                    </div><!-- End .price-box -->
+                                </div><!-- End .product-details -->
+                            </div>
+                        </div>
+                    </div>
+                            <!-- End phần ing  -->
+
+
+
+                            
+                        <!-- Phần thêm vào chữ  -->
+            <h2 class="section-title text-center text-uppercase appear-animate"
+                        data-animation-name="fadeInUpShorter" data-animation-delay="200">Chanel
+                        </h2>
+                    <p style="font-style:italic;" class="section-description text-center mb-3 appear-animate" data-animation-name="fadeInUpShorter"
+                        data-animation-delay="400">"Hương thơm cao cấp, sang trọng, huyền bí"
+                    </p>
+                   <!--End phần thêm chữ vào index  -->
+
+                    <div class="row appear-animate" data-animation-name="fadeInUpShorter" data-animation-delay="400">
+                    
                         <div class="col-6 col-md-4 col-xl-3">
                             <div class="product-default inner-quickview inner-icon">
                                 <figure>
