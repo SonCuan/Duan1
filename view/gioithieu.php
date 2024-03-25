9ee58f87bf0a32914dbce4bc04920352eaf5819d
hoan <hoanthinh311@gmail.com>
2024-03-25 17:52:10 +0700

    update

--- view/gioithieu.php
+++ view/gioithieu.php
@@ -13,13 +13,14 @@
     }
     .title h2{
         font-family: Raleway;
+        
     }
     .title p{
-        margin :0;
-        line-height: 1.715;
+        margin :0 auto;
+        line-height: 20px;
     }
     .title-5c{
-        line-height: 1.5;
+        line-height: 1.25;
     }
   
 </style>
@@ -28,19 +29,19 @@
     <div class="title">
         <h2>VỀ CHÚNG TÔI</h2>
         <p>Mỗi vùng đất mỗi vùng văn hóa đều có những nét đặc trưng riêng điều ấy không chỉ phác thảo bằng những giá trị hữu hình mà còn ẩn chứa trong đó những giá trị vô hình, và ta cảm nhận bằng tâm hồn, trái tim trong đó nổi bật có thể kể đến đó là mùi hương. Tại Porto  chúng tôi tạo ra các mùi hương đa dạng theo phong cách của bạn. Porto Shop cố gắng lột tả hình ảnh văn hóa, tính cách mang đến giá trị cho khách hàng sử dụng.</p>
-    </div>
+    </div><br>
     <div class="title">
         <h2>SỨ MỆNH</h2>
         <p>“Mã số bí mật tô điểm mùi hương cho mỗi người”. Với mong muốn giúp với các bạn tạo dựng nên một phong cách cá nhân riêng. Porto là chìa khóa bí mật giúp bạn thu hút, hấp dẫn và tự tin hơn. Với mục tiêu và sứ mệnh, Porto  mong rằng có thể đưa các sản phẩm nước hoa, những mùi hương được giữ nguyên vẹn tinh thần về với người tiêu dùng Việt Nam khắp cả nước. Với mức giá hợp túi tiền, đảm bảo chất lượng, dịch vụ tư vấn và sự trân trọng đối với người tiêu dùng. Giúp bạn có những trải nghiệm an tâm nhất khi lựa chọn sản phẩm tại Porto. Dựa trên phương châm “Chân thật từ lời nói đến mùi hương”.Porto luôn cố gắng đem đến những gì tốt nhất giúp bảo toàn cảm xúc sử dụng nước hoa của bạn. Bằng cách liên tục nâng cao kiến thức tư vấn cũng như sự chu đáo trong phong cách phục vụ.</p>
-    </div>
+    </div><br>
     <div class="title">
         <h2>SẢN PHẨM Porto</h2>
         <p>Thương hiệu Việt với sản phẩm chủ lực về tinh dầu nước hoa có đa dạng mùi hương dành cho nam, nữ, unisex. Sản phẩm dung tích chai 100ml được đa số khách hàng yêu thích và sử dụng bởi sự nhỏ gọn, tiện lợi, tính đa dụng khi có thể dễ dàng mang theo bên mình, sử dụng bất cứ nơi đâu. Phù hợp với những người có cuộc sống năng động, bận rộn, di chuyển nhiều. Hơn thế nữa, Porto với mùi hương phong phú, độc đáo và mới mẻ giúp thỏa mãn cá tính người dùng khó tính nhất nhưng với một mức giá dễ tiếp cận cho nhiều người. Chúng tôi gửi gắm vào từng sản phẩm với thông điệp “mỗi gói hàng như một gói quà”.</p>
-    </div>
+    </div><br>
     <div class="title">
         <h2>TẦM NHÌN</h2>
         <p>Trở thành một Chuyên gia trong ngành kinh doanh nước hoa. Đưa những điều chân thật nhất đến với người tiêu dùng, đặc biệt là những người mới tham gia vào cộng đồng yêu mùi hương. Giúp cho 1 triệu người Việt Nam tìm thấy được mùi hương yêu thích của mình..</p>
-    </div>
+    </div><br>  
     <div class="title-5c">
         <h2>GIÁ TRỊ CỐT LÕI 5C</h2>
         <h4>Chân thật</h4>
